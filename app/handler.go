a776e4ebee8fa10a5e12f9cb054f916d24823d66
osawa-koki <koki@osawa.dev>
2023-08-12 00:43:45 +0900

    公式コードの実行。 https://aws.github.io/aws-sdk-go-v2/docs/getting-started/

--- app/handler.go
+++ app/handler.go
@@ -1,11 +1,33 @@
 package app
 
 import (
-	"fmt"
-	"os"
+	"context"
+	"log"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
 )
 
 func Handler() {
-	os.Setenv("FOO", "1")
-	fmt.Println("Hello World!!!")
+	// Load the Shared AWS Configuration (~/.aws/config)
+	cfg, err := config.LoadDefaultConfig(context.TODO())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Create an Amazon S3 service client
+	client := s3.NewFromConfig(cfg)
+
+	// Get the first page of results for ListObjectsV2 for a bucket
+	output, err := client.ListObjectsV2(context.TODO(), &s3.ListObjectsV2Input{
+		Bucket: aws.String("s3-manipulator-go-s3-bucket"),
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("first page results:")
+	for _, object := range output.Contents {
+		log.Printf("key=%s size=%d", aws.ToString(object.Key), object.Size)
+	}
 }
