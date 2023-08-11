4385d1a9c18750b0461c5b124deea64b4dcb318e
osawa-koki <koki@osawa.dev>
2023-08-12 02:22:12 +0900

    登録するデータをJSONに。

--- app/putObject.go
+++ app/putObject.go
@@ -3,6 +3,7 @@ package app
 import (
 	"bytes"
 	"context"
+	"encoding/json"
 	"fmt"
 	"log"
 	"time"
@@ -11,19 +12,74 @@ import (
 	"github.com/aws/aws-sdk-go-v2/service/s3"
 )
 
+var (
+	prefixes = []string{
+		"cute",
+		"cool",
+		"beautiful",
+		"pretty",
+		"lovely",
+		"adorable",
+		"gorgeous",
+		"charming",
+		"attractive",
+		"elegant",
+		"exquisite",
+		"magnificent",
+		"delicate",
+		"graceful",
+		"pleasing",
+	}
+	animals = []string{
+		"cat",
+		"dog",
+		"rabbit",
+		"hamster",
+		"goldfish",
+		"turtle",
+		"bird",
+		"snake",
+		"fish",
+		"mouse",
+		"pig",
+		"cow",
+		"sheep",
+		"chicken",
+		"horse",
+		"duck",
+	}
+)
+
+func getBodyMessageString() string {
+	return fmt.Sprintf("%s %s", prefixes[time.Now().Unix()%int64(len(prefixes))], animals[time.Now().Unix()%int64(len(animals))])
+}
+
 func getKeyString() string {
 	date := time.Now()
 	layout := "2006_01_02_15_04_05"
 	return fmt.Sprintf("%s.txt", date.Format(layout))
 }
 
+type Body struct {
+	Message   string    `json:"message"`
+	Timestamp time.Time `json:"timestamp"`
+}
+
 func putObject() {
 	// Create a new Amazon S3 bucket
-	body := bytes.NewReader([]byte("body"))
-	_, err := S3_CLIENT.PutObject(context.TODO(), &s3.PutObjectInput{
+	bodyMessage := getBodyMessageString()
+	body := Body{
+		Message:   bodyMessage,
+		Timestamp: time.Now(),
+	}
+	jsonBody, err := json.Marshal(body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	_, err = S3_CLIENT.PutObject(context.TODO(), &s3.PutObjectInput{
 		Bucket: aws.String(S3_BUCKET_NAME),
 		Key:    aws.String(getKeyString()),
-		Body:   body,
+		Body:   bytes.NewReader(jsonBody),
 	})
 	if err != nil {
 		log.Fatal(err)
